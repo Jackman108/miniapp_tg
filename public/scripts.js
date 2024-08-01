7dae734803ec52f33594d066614afb07b479fb10
devonkidev <dev.js.eugene@gmail.com>
2024-08-01 11:12:10 +0300

    FIX

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,13 @@
 window.Telegram.WebApp.ready();
 
-// Получаем данные пользователя из Telegram WebApp
-const urlParams = new URLSearchParams(window.location.search);
-const usernameFromUrl = urlParams.get('username');
-
 const user = window.Telegram.WebApp.initDataUnsafe.user || {};
 const { photo_url, is_premium } = user;
 
 // Обновляем данные на странице
 document.getElementById('username').innerText = usernameFromUrl || user.username;
 document.getElementById('avatar').src = photo_url || 'https://i.shgcdn.com/432a91c0-438c-4aea-9581-6015be274fe0/-/format/auto/-/preview/3000x3000/-/quality/lighter/';
-if (is_premium) {
-    document.getElementById('premium-icon').style.display = 'inline';
-} else {
-    document.getElementById('premium-icon').style.display = 'none';
-}
+document.getElementById('premium-icon').style.display = is_premium ? 'inline' : 'none';
+
 
 function inviteFriend() {
     const text = 'Привет! Я использую мини-приложение и хотел бы, чтобы ты тоже присоединился. Нажми на ссылку, чтобы открыть приложение: ';
