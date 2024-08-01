2fffe458d0220f26c2a79dc8048715bf37d3c2dd
devonkidev <dev.js.eugene@gmail.com>
2024-08-01 10:40:58 +0300

    fix

--- public/scripts.js
+++ public/scripts.js
@@ -1,23 +1,23 @@
 window.Telegram.WebApp.ready();
 
-const initData = window.Telegram.WebApp.initData || '';
-const urlParams = new URLSearchParams(initData);
+// Получаем данные пользователя из Telegram WebApp
+const urlParams = new URLSearchParams(window.location.search);
+const usernameFromUrl = urlParams.get('username');
 
-// Проверяем наличие данных о пользователе
-const userData = JSON.parse(decodeURIComponent(urlParams.get('user') || '{}'));
-const hasPhotoUrl = userData.photo_url;
-
-console.log('Init Data:', initData);
-console.log('User Data:', userData);
-console.log('Has Photo URL:', hasPhotoUrl);
+const user = window.Telegram.WebApp.initDataUnsafe.user || {};
+const { photo_url, is_premium } = user;
 
 // Обновляем данные на странице
-document.getElementById('username').innerText = userData.username || 'Имя пользователя';
-document.getElementById('avatar').src = hasPhotoUrl ? userData.photo_url : 'https://i.shgcdn.com/432a91c0-438c-4aea-9581-6015be274fe0/-/format/auto/-/preview/3000x3000/-/quality/lighter/';
-document.getElementById('premium-icon').style.display = userData.is_premium ? 'inline' : 'none';
+document.getElementById('username').innerText = usernameFromUrl || user.username;
+document.getElementById('avatar').src = photo_url || 'https://i.shgcdn.com/432a91c0-438c-4aea-9581-6015be274fe0/-/format/auto/-/preview/3000x3000/-/quality/lighter/';
+if (is_premium) {
+    document.getElementById('premium-icon').style.display = 'inline';
+} else {
+    document.getElementById('premium-icon').style.display = 'none';
+}
 
 function inviteFriend() {
-    const text = 'Привет! Я использую мини-приложение и хотел бы, чтобы ты тоже присоединился. Нажми на ссылку, чтобы открыть приложение ';
+    const text = 'Привет! Я использую мини-приложение и хотел бы, чтобы ты тоже присоединился. Нажми на ссылку, чтобы открыть приложение: ';
     const inviteLink = `https://t.me/share/url?url=${encodeURIComponent(window.location.href)}&text=${encodeURIComponent(text)}`;
         window.Telegram.WebApp.openTelegramLink(inviteLink);
 }
